5388fb9a731020b62344d0c8298dfe8aa68eaa82
Yoel Otniel P.M.T (200710753) <yoeltampubolon457@gmail.com>
2022-09-15 14:44:25 +0700

    View Binding

---
+++ app/src/main/java/com/example/view_binding_10753/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10753
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha"),
+        Task("Chapter 3", "Ngopi", "Sehabis Nguli, Marilah Kita Ngopi"),
+        Task("Chapter 4", "Dimana Mama Ai?", "Buat Adu Mekanik"),
+        Task("Chapter 5", "Upload TikTok", "Buat Pamer ke view Showcase Ayaka Pyro C6")
+    )
+}
